1d4248c00d2ca4da41543e169ffd68fa40ecdbf6
aizathamizuddin <aizathamizuddin@gmail.com>
2022-12-01 22:38:20 +0900

    Change for profile button

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,10 @@
 //for main.html
 
+function openProfile(){
+    alert("You haven't signed in. Please login to your account.");
+    openLogin();
+}
+
 function openLogin() {
     window.open("login.html", "_self");
 }
@@ -14,4 +19,28 @@ function openAskQuestion() {
 
 function openAbout() {
     window.open("about.html", "_self");
+}
+
+function openSettings(){
+    alert("Sorry, the Settings page is not ready for use.");
+}
+
+function fnBrowserDetect(){
+    let userAgent = navigator.userAgent;
+    let browserName;
+    
+    if(userAgent.match(/edg/i)){
+        browserName = "edge";
+    }else if(userAgent.match(/firefox|fxios/i)){
+        browserName = "firefox";
+    }  else if(userAgent.match(/safari/i)){
+        browserName = "safari";
+    }else if(userAgent.match(/opr\//i)){
+        browserName = "opera";
+    } else if(userAgent.match(/chrome|chromium|crios/i)){
+        browserName = "chrome";
+    }else{
+        browserName="No browser detection";
+    }
+    alert("You are using " + browserName + " browser");
 }
