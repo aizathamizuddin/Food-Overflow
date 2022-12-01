24cd95b6da9499a4ea3044083800acec135eb27d
aizathamizuddin <aizathamizuddin@gmail.com>
2022-12-02 03:13:23 +0900

    Fix links in JavaScript Navigation

---
+++ JS/pages.js
@@ -0,0 +1,50 @@
+function openUserProfile(){
+    alert("You haven't signed in. Please login to your account.");
+}
+
+function openProfile(){
+    window.open("profile.html", "_self");
+}
+
+function openLogin() {
+    window.open("login.html", "_self");
+}
+
+function openSignup() {
+    window.open("signup.html", "_self");
+}
+
+function openAskQuestion() {
+    window.open("askquestion.html", "_self");
+}
+
+function openAbout() {
+    window.open("about.html", "_self");
+}
+
+function openFAQ() {
+    window.open("faq.html", "_self");
+}
+function openSettings(){
+    alert("Sorry, the Settings page is not ready for use.");
+}
+
+function fnBrowserDetect(){
+    let userAgent = navigator.userAgent;
+    let browserName;
+    
+    if(userAgent.match(/edg/i)){
+        browserName = "edge";
+    }else if(userAgent.match(/firefox|fxios/i)){
+        browserName = "firefox";
+    }  else if(userAgent.match(/safari/i)){
+        browserName = "safari";
+    }else if(userAgent.match(/opr\//i)){
+        browserName = "opera";
+    } else if(userAgent.match(/chrome|chromium|crios/i)){
+        browserName = "chrome";
+    }else{
+        browserName="No browser detection";
+    }
+    alert("You are using " + browserName + " browser");
+}
